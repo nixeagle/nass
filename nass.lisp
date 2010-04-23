36ee3736f22889ebc2c91d50eed1d14ef3f8a66b
James S <i@nixeagle.org>
2010-04-23 15:15:00 -0400

    Handle 0 as a special case as LOG can't handle it.

--- nass.lisp
+++ nass.lisp
@@ -121,8 +121,10 @@ inline with what is expected of `coerce'."
 (defun integer->bit-base-list
     (integer bits &key size (endian :little-endian)
      &aux (size (or size
-                    (floor (log (1+ (abs integer))
-                                (expt 2 bits))))))
+                    (floor (log (abs (if (zerop integer)
+                                         1 ;Prevent divide by 0
+                                         integer))
+                                 (expt 2 bits))))))
   "Convert INTEGER to base that is 2^BITS.
 
 The result list will be SIZE long."
