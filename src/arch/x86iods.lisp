1667b25e9c73620c52a175292551ee54bcc3c47c
James S <i@nixeagle.org>
2010-05-09 14:42:51 -0400

    Art of assembly calls this the mod-reg-r/m bit

--- src/arch/x86iods.lisp
+++ src/arch/x86iods.lisp
@@ -2,7 +2,9 @@
 
 (define-binary-class x86oid (little-endian) ())
 
-(define-binary-class mod-r/m (x86oid)
+(define-binary-class mod-reg-r/m (x86oid)
     ((mod :bits 2 :initarg :mod :type (mod 4))
      (r/m :bits 3 :initarg :r/m :type (mod 8))
      (reg/opcode :bits 3 :initarg :reg/opcode :type (mod 8))))
+
+;;; END
