761db761417d22ca088c662e01e5860452822bfa
James S <i@nixeagle.org>
2010-05-08 22:05:45 -0400

    define generic function OPCODE

--- src/abstract.lisp
+++ src/abstract.lisp
@@ -8,3 +8,9 @@
 
 Anything to be used as a _primary_ opcode for a machine instruction should
 make sure this class is in the superclass list."))
+
+;;; This being an integer is an arbitrary specification Lets try to adhere
+;;; to it unless overriding reasons for the _primary_ opcode cannot be
+;;; expressed as a single integer.
+(defgeneric opcode (assembly-object &key &allow-other-keys)
+  (:documentation "Gets ASSEMBLY-OBJECT's opcode as an integer."))
