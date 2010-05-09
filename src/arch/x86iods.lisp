f8cbb30c24e467c00d0c2c8216a7ee31196c7e57
James S <i@nixeagle.org>
2010-05-09 03:30:34 -0400

    Define X86OID and MOD-R/M.

---
+++ src/arch/x86iods.lisp
@@ -0,0 +1,8 @@
+(in-package :nass.arch.x86oids)
+
+(define-binary-class x86oid (little-endian) ())
+
+(define-binary-class mod-r/m (x86oid)
+    ((mod :bits 2 :initarg :mod :type (mod 4))
+     (r/m :bits 3 :initarg :r/m :type (mod 8))
+     (reg/opcode :bits 3 :initarg :reg/opcode :type (mod 8))))
