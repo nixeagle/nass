1a4daa23acf79fd4522eef76b73aed2f35c70721
James S <i@nixeagle.org>
2010-05-10 00:12:44 -0400

    Define types for 8086 segment registers

--- src/arch/i8086/init-i8086.lisp
+++ src/arch/i8086/init-i8086.lisp
@@ -1,4 +1,22 @@
 (in-package :nass.arch.i8086)
 
+(macrolet ((define-type (name full-name &rest members)
+             (let ((documentation
+                    (format nil "Mnemonic ~A stands for ~(~A~)." name
+                            (substitute #\Space #\- (symbol-name full-name)))))
+               `(progn
+                  (deftype ,full-name ()
+                    ,documentation
+                    '(member ,@members))
+                  (deftype ,name ()
+                    ,documentation
+                    ',full-name)))))
+  (define-type es extra-segment 0)
+  (define-type cs code-segment 1)
+  (define-type ds data-segment 2)
+  (define-type ss stack-segment 3))
+
+;;; END
+
 
 
