5c50c3d350b298eb37035dadb59d2541080759cb
James S <i@nixeagle.org>
2010-05-09 22:24:49 -0400

    Define the high and low reg types. These are when w = 0.

--- src/arch/x86iods.lisp
+++ src/arch/x86iods.lisp
@@ -2,29 +2,40 @@
 
 (define-binary-class x86oid (little-endian) ())
 
-(macrolet ((define-type (name full-name &rest members)
-             (declare (symbol name full-name))
+(macrolet ((define-type (name full-name width &rest members)
+             (declare (symbol name full-name)
+                      ((unsigned-byte 1) width))
              (let ((documentation
                     (format nil "Mnemonic ~A stands for ~(~A~).~
-                                ~%WIDTH must be 1." name
-                                (substitute #\Space #\- (symbol-name full-name)))))
+                                ~%~%WIDTH must be ~D." name
+                                (substitute #\Space #\- (symbol-name full-name))
+                                width)))
                `(progn
-                  (deftype ,full-name (&key (width 1))
+                  (deftype ,full-name (&key (width ,width))
                     ,documentation
-                    (when (= width 1)
+                    (when (= width ,width)
                       '(member ,@members)))
-                  (deftype ,name (&key (width 1))
+                  (deftype ,name (&key (width ,width))
                     ,documentation
-                    (when (= width 1)
+                    (when (= width ,width)
                       ',full-name))))))
-  (define-type ax accumulator-register 0)
-  (define-type bx base-address-register 3)
-  (define-type cx count-register 1)
-  (define-type dx data-register 2)
-  (define-type sp stack-pointer 4)
-  (define-type bp base-pointer 5)
-  (define-type si string-index 6)
-  (define-type di data-index 7))
+  (define-type ax accumulator-register 1 0)
+  (define-type bx base-address-register 1 3)
+  (define-type cx count-register 1 1)
+  (define-type dx data-register 1 2)
+  (define-type sp stack-pointer 1 4)
+  (define-type bp base-pointer 1 5)
+  (define-type si string-index 1 6)
+  (define-type di data-index 1 7)
+  ;; low and high bits
+  (define-type al low-accumulator-register 0 0)
+  (define-type bl low-base-address-register 0 3)
+  (define-type cl low-count-register 0 1)
+  (define-type dl low-data-register 0 2)
+  (define-type ah high-accumulator-register 0 4)
+  (define-type bh high-base-address-register 0 7)
+  (define-type ch high-count-register 0 5)
+  (define-type dl high-data-register 0 6))
 
 (define-binary-class mod-reg-r/m (x86oid)
   ((mod :bits 2 :initarg :mod :type (mod 4))
