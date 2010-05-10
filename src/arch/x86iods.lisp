45774ed49d9f723a7d1567bd600b2ad9471d0be5
James S <i@nixeagle.org>
2010-05-09 20:05:30 -0400

    reindent now that emacs wants to behave right today.

--- src/arch/x86iods.lisp
+++ src/arch/x86iods.lisp
@@ -3,8 +3,8 @@
 (define-binary-class x86oid (little-endian) ())
 
 (define-binary-class mod-reg-r/m (x86oid)
-    ((mod :bits 2 :initarg :mod :type (mod 4))
-     (r/m :bits 3 :initarg :r/m :type (mod 8))
-     (reg/opcode :bits 3 :initarg :reg/opcode :type (mod 8))))
+  ((mod :bits 2 :initarg :mod :type (mod 4))
+   (r/m :bits 3 :initarg :r/m :type (mod 8))
+   (reg/opcode :bits 3 :initarg :reg/opcode :type (mod 8))))
 
 ;;; END
