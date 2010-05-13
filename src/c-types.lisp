6a21ec5d6ca7f434a732c139b5dd01ac7b05018b
James S <i@nixeagle.org>
2010-05-13 17:08:18 -0400

    define SHORT-INT

--- src/c-types.lisp
+++ src/c-types.lisp
@@ -1,6 +1,11 @@
+;;; These are currently x86oid based.
 (in-package :nass.ctypes)
 
 (deftype char ()
   "C chars are always 1 byte."
   '(octet 1))
+
+(deftype short-int ()
+  "C short int is 2 bytes long on all versions."
+  '(octet 2))
 
