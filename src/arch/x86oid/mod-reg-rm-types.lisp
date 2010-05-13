efa73d31c4b2a7edfebe15fc26acefc415ea8732
James S <i@nixeagle.org>
2010-05-13 19:31:20 -0400

    More mod-reg-r/m types.
    Some of these I don't have a clue what they are named for

--- src/arch/x86oid/mod-reg-rm-types.lisp
+++ src/arch/x86oid/mod-reg-rm-types.lisp
@@ -13,3 +13,16 @@
 (deftype r32 ()
   "32 bit registers."
   '(member :eax :ecx :edx :ebx :esp :ebp :esi :edi))
+
+(deftype mm ()
+  '(member :mm0 :mm1 :mm2 :mm3 :mm4 :mm5 :mm6 :mm7))
+
+(deftype xmm ()
+  '(member :xmm0 :xmm1 :xmm2 :xmm3 :xmm4 :xmm5 :xmm6 :xmm7))
+
+(deftype 16-bit-segment-register ()
+  "sreg: Segment register names."
+  '(member :es :cs :ss :ds :fs :gs))
+
+(deftype eee ()
+  '(member :cr0 :cr2 :cr3 :cr4 :dr0 :dr1 :dr2 :dr3 :dr4 :dr5 :dr6 :dr7))
