cba3948bf8c081be3ebce53718d91ef2233467d9
James S <i@nixeagle.org>
2010-05-13 22:36:54 -0400

    Define ENCODE-REG-BITS

--- src/arch/x86oid/mod-reg-rm-types.lisp
+++ src/arch/x86oid/mod-reg-rm-types.lisp
@@ -26,3 +26,50 @@
 
 (deftype eee ()
   '(member :cr0 :cr2 :cr3 :cr4 :dr0 :dr1 :dr2 :dr3 :dr4 :dr5 :dr6 :dr7))
+
+(deftype mod-rem-r/m-register ()
+  "All valid x86oid register names."
+  '(or r8 r16 r32 mm xmm 16-bit-segment-register eee
+    ;; FIXME: These below should become their own subtypes
+    (member :r8b :r9b :r10b :r11b :r12b :r13b :r14b :r15b
+     :r8w :r9w :r10w :r11w :r12w :r13w :r14w :r15w
+     :r8d :r9d :r10d :r11d :r12d :r13d :r14d :r15d
+     :r8 :r9 :r10 :r11 :r12 :r13 :r14 :r15
+     :XMM8 :XMM9 :XMM10 :XMM11 :XMM12 :XMM13 :XMM14 :XMM15
+     :cr8)))
+
+;;; OPTIMIZE: If anyone cares or thinks this is too slow: Put these in the
+;;; order of expected use. The items closer to the front of this array
+;;; will be looked up faster then the items at the end. So put the more
+;;; common instances at the front, however! do not change the order of the
+;;; rows themselves.
+(defparameter +register-list+
+  #(:al :cl :dl :bl :ah :ch :dh :bh     ;16 and 32 bit regs
+    :ax :cx :dx :bx :sp :bp :si :di
+    :eax :ecx :edx :ebx :esp :ebp :esi :edi
+    :mm0 :mm1 :mm2 :mm3 :mm4 :mm5 :mm6 :mm7
+    :xmm0 :xmm1 :xmm2 :xmm3 :xmm4 :xmm5 :xmm6 :xmm7
+    :es :cs :ss :ds :fs :gs :reserved :reserved
+    :cr0 :invalid :cr2 :cr3 :cr4 :invalid :invalid :invalid
+    :dr0 :dr1 :dr2 :dr3 :dr4 :dr5 :dr6 :dr7
+    ;; These below apply only for amd64 arch.
+    :r8b :r9b :r10b :r11b :r12b :r13b :r14b :r15b
+    :r8w :r9w :r10w :r11w :r12w :r13w :r14w :r15w
+    :r8d :r9d :r10d :r11d :r12d :r13d :r14d :r15d
+    :r8 :r9 :r10 :r11 :r12 :r13 :r14 :r15
+    :XMM8 :XMM9 :XMM10 :XMM11 :XMM12 :XMM13 :XMM14 :XMM15
+    :cr8 :invalid :invalid :invalid :invalid :invalid :invalid :invalid)
+  "table of all valid x86oid registers.
+
+These are in order such that:
+  (mod (position keyword `+register-list+') 8)
+will return the correct bit sequence as an integer.")
+
+(defun encode-reg-bits (reg-name)
+  "Compute 3 bit number corresponding to REG-NAME."
+  (declare (mod-rem-r/m-register reg-name)
+           (optimize (speed 3) (space 0)))
+  (mod (position reg-name (the simple-vector +register-list+)) 8))
+
+
+;;; END
