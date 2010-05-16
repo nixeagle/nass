ad67f5dcfcfd0b99c4033eb4bb94933b676c980f
James S <i@nixeagle.org>
2010-05-16 01:34:11 -0400

    Define NASM-STRING

--- nass.lisp
+++ nass.lisp
@@ -24,6 +24,8 @@
 (defvar *ndisasm-test-file-path*
   "/tmp/nass-ndisam-test")
 
+(defvar *nasm-test-file-path*
+  "/tmp/nass-nasm-test.S")
 
 (defmacro ndisasm-binary ((s &optional (path *ndisasm-test-file-path*))
                           &body body)
@@ -34,4 +36,13 @@
            `(funcall #',(find-symbol "SHELL-COMMAND" :trivial-shell)
                      ,(format nil "ndisasm ~A" path))
            `(error "Requires trivial-shell!"))))
+
+(defun nasm-string (asm-instructions &key (file *nasm-test-file-path*))
+  (declare (string asm-instructions))
+  (with-open-file (s file
+                     :direction :output
+                     :if-exists :supersede
+                     :if-does-not-exist :create)
+    (princ asm-instructions s))
+  (trivial-shell:shell-command (format nil "nasm -fbin -l ~A.listing ~A -o ~A.out && cat ~A.listing" file file file file)))
 ;;; END
