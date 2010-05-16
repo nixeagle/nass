3773ee5bf51b26fa8390525232beb77892e26fba
James S <i@nixeagle.org>
2010-05-16 14:11:10 -0400

    Define *ARCHITECTURE* and *INSTRUCTION-SET*

--- src/global-state.lisp
+++ src/global-state.lisp
@@ -5,3 +5,16 @@
 
 This defaults to 16 bits for now as its the simplest x86 machine size.")
 
+(defvar *architecture* :x86
+  "Type of machine.
+
+  - :x86 is all the various i8086 derived things.
+  - others might be: :arm :mips :ppc")
+
+(defvar *instruction-set* '()
+  "List of additional instruction sets that should be supported. If the
+set is not in the list the assembler will not use the instruction set
+and will signal an error.")
+
+;;; END
+
