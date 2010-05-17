e3c31909cf2eed9f32388ddc7d3c6ca94706f0ff
James S <i@nixeagle.org>
2010-05-17 00:39:21 -0400

    Add :general to *INSTRUCTION-SET*

--- src/global-state.lisp
+++ src/global-state.lisp
@@ -17,7 +17,7 @@ This defaults to 16 bits for now as its the simplest x86 machine size.")
   - :x86 is all the various i8086 derived things.
   - others might be: :arm :mips :ppc")
 
-(defvar *instruction-set* '()
+(defvar *instruction-set* '(:general)
   "List of additional instruction sets that should be supported. If the
 set is not in the list the assembler will not use the instruction set
 and will signal an error.")
